e2cfbcdd5c7bb1641fa4a53cac88f8ba2f69a715
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-02-15 14:13:38 -0300

    add genre route to express app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import {movieRoutes} from "./routes/MovieRoutes";
+import {genreRoutes} from "./routes/GenreRoutes";
 
 export const app = express();
 app.use(express.json());
 app.use(movieRoutes);
+app.use(genreRoutes)
 
